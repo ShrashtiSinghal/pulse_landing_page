5838ddfe1a09f423a2ff4c7ef4e9b63af2fa915c
Shrashti Singhal <shrashtisinghal@gmail.com>
2025-11-04 15:33:34 +0530

    Add favicon links, update routing for production, change Docker port to 3001

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import LandingPage from './pages/LandingPage';
 import './App.css';
 
 function App() {
+  // Get basename from environment or default to root
+  const basename = process.env.PUBLIC_URL || '';
+  
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          {/* Catch-all route for SPA - redirects to home */}
+          <Route path="*" element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
